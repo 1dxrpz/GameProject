3933dbccc1874d789241a8f191071d5b41a6f69b
Kenneth Gibson <scaphoidederps@gmail.com>
2021-07-10 06:31:01 +0300

    controls

--- UniversityGameProject/Assets/Scripts/PlayerController.cs
+++ UniversityGameProject/Assets/Scripts/PlayerController.cs
@@ -133,7 +133,9 @@ public class PlayerController : MonoBehaviour
                 }
 			}
         }
-        ButtonDown = Input.anyKey;
+
+        ButtonDown = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S);
         
         Direction = Vector3.Normalize(Direction);
         
