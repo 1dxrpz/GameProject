e7cf201185ca6e2179fad2554cdee591132356f9
Ilya <ilia78-03@mail.ru>
2021-07-12 00:48:42 +0300

    asdas
    asdasd

--- UniversityGameProject/Assets/Scripts/LightForBush.cs
+++ UniversityGameProject/Assets/Scripts/LightForBush.cs
@@ -1,15 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LightForBush : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+{ 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
--- UniversityGameProject/Assets/triggerzoneDetect.cs
+++ UniversityGameProject/Assets/triggerzoneDetect.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class triggerzoneDetect : MonoBehaviour
@@ -11,25 +9,20 @@ public class triggerzoneDetect : MonoBehaviour
     {
         counter++;
         GetComponent<Renderer>().material.color += new Color(0, 0, 0, 0.3f * Time.deltaTime);
+
         if (PlayerInZone && counter >= 200)
-        {
             GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(-5f, 0.93f, 7f);
-        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
-        {
             PlayerInZone = true;
-        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
-        {
             PlayerInZone = false;
-        }
     }
 }
