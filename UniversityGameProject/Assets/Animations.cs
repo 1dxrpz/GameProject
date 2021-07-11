ad0919f8abf0abd63ba454270faf74c9995851fe
Ilya <ilia78-03@mail.ru>
2021-07-12 02:49:36 +0300

    asdasd
    asdasd

---
+++ UniversityGameProject/Assets/Animations.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    private Vector3 memVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
